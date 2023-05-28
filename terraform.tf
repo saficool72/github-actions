9253c5f41f07508ceb5d0266d163a32cf19bfb31
saficool72 <96882570+saficool72@users.noreply.github.com>
2023-05-28 20:45:53 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "new-vpc2" {
+cidr_block = "10.0.0.0/16"
+}
