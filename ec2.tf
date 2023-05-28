b2c6d3255c04195a8f8601025c47844e2e630eb0
saficool72 <96882570+saficool72@users.noreply.github.com>
2023-05-28 22:03:51 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -20,15 +20,6 @@ resource "aws_instance" "web" {
   ami           = "ami-0715c1897453cabd1"
   subnet_id     = aws_subnet.my_subnet.id
   instance_type = "t2.micro"
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
-
 
   tags = {
     Name = "HelloWorld-2"
