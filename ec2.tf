eb30c1867c8dd519b8b8d116fb100152cb786937
saficool72 <96882570+saficool72@users.noreply.github.com>
2023-05-28 21:58:57 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,26 @@
-data "aws_ami_ids" "ubuntu" {
-  owners = ["236233967963"]
+resource "aws_vpc" "myvpc" {
+  cidr_block = "172.16.0.0/16"
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/ubuntu-*-*-amd64-server-*"]
+  tags = {
+    Name = "tf-myvpc"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "tf-mysubnet"
   }
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami_ids.ubuntu.id
+  ami           = "ami-0715c1897453cabd1"
+  subnet_id     = aws_subnet.my_subnet.id
   instance_type = "t2.micro"
-    user_data = <<-EOF
+  user_data = <<-EOF
               #!/bin/bash
               apt-get update
               apt-get install -y apache2
@@ -19,7 +29,8 @@ resource "aws_instance" "web" {
               systemctl restart apache2
               EOF
 
+
   tags = {
-    Name = "HelloWorld"
+    Name = "HelloWorld-2"
   }
 }
